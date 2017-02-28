bd29010f6ecd55daf7b08375f76a79b3779f1aed
KVRaghava93 <raghava.6464@gmail.com>
2017-02-27 18:05:16 -0600

    programs using sample sports videos

---
+++ Source/phase 1 programs/image_classification_Windows/src/main/scala/ResizingImage.scala
@@ -0,0 +1,38 @@
+import java.io.File
+
+import org.bytedeco.javacpp.opencv_core.{Size, Mat}
+import org.bytedeco.javacpp.opencv_highgui._
+
+/**
+  * Created by Mayanka on 16-Mar-16.
+  */
+object ResizingImage {
+
+  def main(args: Array[String]) {
+    //val IMAGE_CATEGORIES = List("rice", "tempura", "toast", "bibimap", "sushi", "spaghetti", "sausage", "oden", "omelet", "jiaozi")
+    //val IMAGE_CATEGORIES = List("accordion", "airplanes", "anchor", "ant", "barrel", "bass", "beaver", "binocular", "bonsai")
+    //val IMAGE_CATEGORIES = List("airplane", "apple", "cats", "eggs", "noodles", "wolves")
+    val IMAGE_CATEGORIES = List("cricket", "tennis","baseball","soccer")
+    val PATH = "data3/test/"
+    val PATH2 = "data3/test2/"
+    IMAGE_CATEGORIES.foreach(f => {
+      val file = new File(PATH + f)
+      val listOffiles = file.listFiles()
+      val file2 = new File(PATH2 + f)
+      file2.mkdirs()
+      var count = 1
+      listOffiles.foreach(fi => {
+        println(fi.getPath)
+        val img = imread(fi.getPath, CV_LOAD_IMAGE_UNCHANGED)
+        //src image
+        var src = new Mat
+        //src = img.reshape(img.channels(), (img.rows() / 4))
+        org.bytedeco.javacpp.opencv_imgproc.resize(img,src,new Size(img.rows()/2,img.cols()/2))
+
+
+        imwrite(file2.getPath + "/" + count + ".jpg", src)
+        count = count + 1
+      })
+    })
+  }
+}
